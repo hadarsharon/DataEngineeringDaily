1d816b9319b51800cbd3363a781bbd0ee5c1cb24
hadarsharon <hadar.sharon94@gmail.com>
2021-03-29 02:50:55 +0300

    A lot of work, a lot

--- js/quizzes.js
+++ js/quizzes.js
@@ -3,7 +3,7 @@ let templateObjectsQuizzes = [
         template: '#quizzes-handlebars-template',
         context: {
             "quizButtons": [
-                {"button-class": "btn-info", "button-level": "Basic", "quiz-modal": "basic-quiz-modal"},
+                {"button-class": "btn-info", "button-level": "Basic", "quiz-modal": "basic-quiz"},
                 {"button-class": "btn-primary", "button-level": "Novice"},
                 {"button-class": "btn-success", "button-level": "Intermediate"},
                 {"button-class": "btn-warning", "button-level": "Advanced"},
@@ -17,64 +17,53 @@ let templateObjectsQuizzes = [
         context: {
             "quizModals": [
                 {
-                    "modal-id": "basic-quiz-modal",
+                    "modal-id": "basic-quiz",
                     "modal-title": "Data Engineering Daily Quiz - <b>Basic</b>",
-                    "modal-content":
-                        `
-                        <ol>
-                            <li>
-                                <h5 class="quiz-question">What is an RDBMS?</h5>
-                                <input type="radio" id="basic_q1a1" name="basic_q1" value=0>
-                                <label for="basic_q1a1">Raw Data Block Management Store</label><br>
-                                <input type="radio" id="basic_q1a2" name="basic_q1" value=0>
-                                <label for="basic_q1a2">Rich Database Migration System</label><br>
-                                <input type="radio" id="basic_q1a3" name="basic_q1" value=1>
-                                <label for="basic_q1a3">Relational Database Management System</label><br>
-                                <input type="radio" id="basic_q1a4" name="basic_q1" value=0>
-                                <label for="basic_q1a4">Regular Data Block Matching System</label>
-                            </li>
-                            <li>
-                                <h5 class="quiz-question">What is the most popular query language among Data Engineers?</h5>
-                                <input type="radio" id="basic_q2a1" name="basic_q2" value=1>
-                                <label for="basic_q2a1">SQL</label><br>
-                                <input type="radio" id="basic_q2a2" name="basic_q2" value=0>
-                                <label for="basic_q2a2">QUEL</label><br>
-                                <input type="radio" id="basic_q2a3" name="basic_q2" value=0>
-                                <label for="basic_q2a3">HiveQL</label><br>
-                                <input type="radio" id="basic_q2a4" name="basic_q2" value=0>
-                                <label for="basic_q2a4">LINQ</label>
-                            </li>
-                            <li>
-                                <h5 class="quiz-question">Which of the following is NOT a common data format?</h5>
-                                <input type="radio" id="basic_q3a1" name="basic_q3" value=0>
-                                <label for="basic_q3a1">JSON</label><br>
-                                <input type="radio" id="basic_q3a2" name="basic_q3" value=0>
-                                <label for="basic_q3a2">CSV</label><br>
-                                <input type="radio" id="basic_q3a3" name="basic_q3" value=0>
-                                <label for="basic_q3a3">XML</label><br>
-                                <input type="radio" id="basic_q3a4" name="basic_q3" value=1>
-                                <label for="basic_q3a4">TCP</label>
-                            </li>
-                            <li>
-                                <h5 class="quiz-question">Which Programming Language is NOT used for Data Engineering?</h5>
-                                <input type="radio" id="basic_q4a1" name="basic_q4" value=0>
-                                <label for="basic_q4a1">Java</label><br>
-                                <input type="radio" id="basic_q4a2" name="basic_q4" value=0>
-                                <label for="basic_q4a2">Python</label><br>
-                                <input type="radio" id="basic_q4a3" name="basic_q4" value=1>
-                                <label for="basic_q4a3">Dart</label><br>
-                                <input type="radio" id="basic_q4a4" name="basic_q4" value=0>
-                                <label for="basic_q4a4">R</label>
-                            </li>
-                            <li>
-                                <h5 class="quiz-question">Many widely-used Data Engineering tools are Open Source</h5>
-                                <input type="radio" id="basic_q5a1" name="basic_q5" value=0>
-                                <label for="basic_q5a1">False</label><br>
-                                <input type="radio" id="basic_q5a2" name="basic_q5" value=1>
-                                <label for="basic_q5a2">True</label><br>
-                            </li>
-                        </ol>
-                        `
+                    "questions": [
+                        {
+                            "question": "What is an RDBMS?",
+                            "answers": [
+                                ["Raw Data Block Management Store"],
+                                ["Rich Database Migration System"],
+                                ["Relational Database Management System", true],
+                                ["Regular Data Block Matching System"]
+                            ]
+                        },
+                        {
+                            "question": "What is the most popular query language among Data Engineers?",
+                            "answers": [
+                                ["SQL", true],
+                                ["QUEL"],
+                                ["HiveQL"],
+                                ["LINQ"]
+                            ]
+                        },
+                        {
+                            "question": "Which of the following is <strong><em>NOT</em></strong> a common data format?",
+                            "answers": [
+                                ["JSON"],
+                                ["CSV"],
+                                ["XML"],
+                                ["TCP", true]
+                            ]
+                        },
+                        {
+                            "question": "Which Programming Language is <strong><em>NOT</em></strong> used for Data Engineering?",
+                            "answers": [
+                                ["Java"],
+                                ["Python"],
+                                ["Dart", true],
+                                ["R"]
+                            ]
+                        },
+                        {
+                            "question": "Many widely-used Data Engineering tools are Open Source",
+                            "answers": [
+                                ["False"],
+                                ["True", true]
+                            ]
+                        },
+                    ]
                 }
             ]
         },
