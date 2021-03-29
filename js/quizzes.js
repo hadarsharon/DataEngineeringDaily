e64907b70fdae6739dff89611f41f10b535f568b
hadarsharon <hadar.sharon94@gmail.com>
2021-03-29 03:16:14 +0300

    eh

--- js/quizzes.js
+++ js/quizzes.js
@@ -1,13 +1,19 @@
+function calculateScore(quiz) {
+    let points = 5;
+    let total = 10;
+    $('#pointsScored').html("Your Score: <strong>" + points + "/" + total + "</strong>");
+}
+
 let templateObjectsQuizzes = [
     {
         template: '#quizzes-handlebars-template',
         context: {
             "quizButtons": [
                 {"button-class": "btn-info", "button-level": "Basic", "quiz-modal": "basic-quiz"},
-                {"button-class": "btn-primary", "button-level": "Novice"},
-                {"button-class": "btn-success", "button-level": "Intermediate"},
-                {"button-class": "btn-warning", "button-level": "Advanced"},
-                {"button-class": "btn-danger", "button-level": "Expert"},
+                {"button-class": "btn-primary", "button-level": "Novice", "quiz-modal": "novice-quiz"},
+                {"button-class": "btn-success", "button-level": "Intermediate", "quiz-modal": "intermediate-quiz"},
+                {"button-class": "btn-warning", "button-level": "Advanced", "quiz-modal": "advanced-quiz"},
+                {"button-class": "btn-danger", "button-level": "Expert", "quiz-modal": "expert-quiz"},
             ]
         },
         target: '#quizzes-buttons'
@@ -64,7 +70,101 @@ let templateObjectsQuizzes = [
                             ]
                         },
                     ]
-                }
+                },
+                {
+                    "modal-id": "novice-quiz",
+                    "modal-title": "Data Engineering Daily Quiz - <b>Novice</b>",
+                    "questions": [
+                        {
+                            "question": "Select the SQL clause for setting an upper bound on the number of possible records to be returned from a query",
+                            "answers": [
+                                ["STOP"],
+                                ["UNTIL"],
+                                ["BOUND"],
+                                ["LIMIT", true]
+                            ]
+                        },
+                        {
+                            "question": "Git is a tool used primarily for Version Management / Version Control",
+                            "answers": [
+                                ["False"],
+                                ["True", true]
+                            ]
+                        },
+                        {
+                            "question": "What SQL clause is used for ordering query results in a descending order?",
+                            "answers": [
+                                ["ORDER BY DESC", true],
+                                ["SORT BY DESC"],
+                                ["DIRECT BY DESC"],
+                                ["There is no such clause, because they are always ordered descending by default."]
+                            ]
+                        },
+                        {
+                            "question": "In Python, whitespace is used to denote blocks, but only cosmetically (i.e. does not affect code execution)",
+                            "answers": [
+                                ["False", true],
+                                ["True"]
+                            ]
+                        },
+                        {
+                            "question": "Select the clause that is <strong><em>NOT</em></strong> a part of ANSI SQL standard",
+                            "answers": [
+                                ["DISTINCT"],
+                                ["PIVOT", true],
+                                ["LIKE"],
+                                ["PARTITION BY"]
+                            ]
+                        },
+                    ]
+                },
+                {
+                    "modal-id": "intermediate-quiz",
+                    "modal-title": "Data Engineering Daily Quiz - <b>Intermediate</b>",
+                    "questions": [
+                        {
+                            "question": "Select the SQL clause for setting an upper bound on the number of possible records to be returned from a query",
+                            "answers": [
+                                ["STOP"],
+                                ["UNTIL"],
+                                ["BOUND"],
+                                ["LIMIT", true]
+                            ]
+                        },
+                        {
+                            "question": "Git is a tool used primarily for Version Management / Version Control",
+                            "answers": [
+                                ["False"],
+                                ["True", true]
+                            ]
+                        },
+                        {
+                            "question": "What SQL clause is used for ordering query results in a descending order?",
+                            "answers": [
+                                ["ORDER BY DESC", true],
+                                ["SORT BY DESC"],
+                                ["DIRECT BY DESC"],
+                                ["There is no such clause, because they are always ordered descending by default."]
+                            ]
+                        },
+                        {
+                            "question": "In Python, whitespace is used to denote blocks, but only cosmetically (i.e. does not affect code execution)",
+                            "answers": [
+                                ["False", true],
+                                ["True"]
+                            ]
+                        },
+                        {
+                            "question": "Select the clause that is <strong><em>NOT</em></strong> a part of ANSI SQL standard",
+                            "answers": [
+                                ["DISTINCT"],
+                                ["PIVOT", true],
+                                ["LIKE"],
+                                ["PARTITION BY"]
+                            ]
+                        },
+                    ]
+                },
             ]
         },
         target: '#quizzes-modals'
