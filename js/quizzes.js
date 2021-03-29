049cde98de4f5d86d5c3f428545b023ad5231020
hadarsharon <hadar.sharon94@gmail.com>
2021-03-29 03:31:11 +0300

    more work

--- js/quizzes.js
+++ js/quizzes.js
@@ -123,44 +123,47 @@ let templateObjectsQuizzes = [
                     "modal-title": "Data Engineering Daily Quiz - <b>Intermediate</b>",
                     "questions": [
                         {
-                            "question": "Select the SQL clause for setting an upper bound on the number of possible records to be returned from a query",
+                            "question": "Which of the following Databases is <strong><em>NOT</em></strong> Open Source?",
                             "answers": [
-                                ["STOP"],
-                                ["UNTIL"],
-                                ["BOUND"],
-                                ["LIMIT", true]
+                                ["PostgreSQL"],
+                                ["MariaDB"],
+                                ["SQLite"],
+                                ["SQL Server", true]
                             ]
                         },
                         {
-                            "question": "Git is a tool used primarily for Version Management / Version Control",
-                            "answers": [
-                                ["False"],
-                                ["True", true]
-                            ]
-                        },
-                        {
-                            "question": "What SQL clause is used for ordering query results in a descending order?",
-                            "answers": [
-                                ["ORDER BY DESC", true],
-                                ["SORT BY DESC"],
-                                ["DIRECT BY DESC"],
-                                ["There is no such clause, because they are always ordered descending by default."]
-                            ]
-                        },
-                        {
-                            "question": "In Python, whitespace is used to denote blocks, but only cosmetically (i.e. does not affect code execution)",
+                            "question": "R is usually faster than Python",
                             "answers": [
                                 ["False", true],
                                 ["True"]
                             ]
                         },
                         {
-                            "question": "Select the clause that is <strong><em>NOT</em></strong> a part of ANSI SQL standard",
+                            "question": "parquet (the file format) is:",
                             "answers": [
-                                ["DISTINCT"],
-                                ["PIVOT", true],
-                                ["LIKE"],
-                                ["PARTITION BY"]
+                                ["Compressed by default and columnar", true],
+                                ["Compressed by default and row-based"],
+                                ["Uncompressed by default and columnar"],
+                                ["Uncompressed by default and row-based"]
+                            ]
+                        },
+                        {
+                            "question": "Avro (the file format) is:",
+                            "answers": [
+                                ["Compressed by default and columnar"],
+                                ["Compressed by default and row-based"],
+                                ["Uncompressed by default and columnar"],
+                                ["Uncompressed by default and row-based", true]
+                            ]
+                        },
+                        {
+                            "question": "Let's assume tables <b>t1</b> and <b>t2</b> have <b>3</b> and <b>5</b> rows respectively." +
+                                "<br>How many records will this query return:<br><strong>SELECT * FROM t1 CROSS JOIN t2;</strong>",
+                            "answers": [
+                                [8],
+                                [15, true],
+                                [125],
+                                [243]
                             ]
                         },
                     ]
